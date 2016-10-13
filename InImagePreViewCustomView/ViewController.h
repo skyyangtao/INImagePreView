82597cd137d9800af29160d1429d01dbf447c95b
yangtao <826750129@qq.com>
2016-10-13 17:22:10 +0800

    Initial Commit

---
+++ InImagePreViewCustomView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InImagePreViewCustomView
+//
+//  Created by 杨涛 on 16/10/13.
+//  Copyright © 2016年 www.mepadi.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InImagePreViewCustomView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  InImagePreViewCustomView
+//
+//  Created by 杨涛 on 16/10/13.
+//  Copyright © 2016年 www.mepadi.me. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
